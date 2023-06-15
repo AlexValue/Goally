aefc8acfa41382b6904c87d9647537880f8c888f
AniaKoriakina <82394052+AniaKoriakina@users.noreply.github.com>
2023-06-15 15:24:52 +0500

    вёрстка страницы "Награды"

--- app/src/main/java/com/example/mygoallyapp/MotivationFragment.kt
+++ app/src/main/java/com/example/mygoallyapp/MotivationFragment.kt
@@ -1,11 +1,18 @@
 package com.example.mygoallyapp
 
+import android.graphics.Color
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
+import android.graphics.PorterDuff
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.example.mygoallyapp.Data.GoalsDatabase
 import kotlinx.coroutines.Dispatchers
@@ -54,6 +61,21 @@ class MotivationFragment : Fragment() {
         val level: TextView = view.findViewById(R.id.level)
         val experience: TextView = view.findViewById(R.id.experience)
 
+        val levelProgressBar: ProgressBar = view.findViewById(R.id.levelProgressBar)
+        val task1ProgressBar : ProgressBar = view.findViewById(R.id.task1ProgressBar)
+        val task2ProgressBar : ProgressBar = view.findViewById(R.id.task2ProgressBar)
+
+        val achievement1ProgressBar : ProgressBar = view.findViewById(R.id.achievement1ProgressBar)
+        val achievement2ProgressBar : ProgressBar = view.findViewById(R.id.achievement2ProgressBar)
+        val achievement3ProgressBar : ProgressBar = view.findViewById(R.id.achievement3ProgressBar)
+
+        val achievement1Image : ImageView = view.findViewById(R.id.achievement1Image)
+        val achievement2Image : ImageView = view.findViewById(R.id.achievement2Image)
+        val achievement3Image : ImageView = view.findViewById(R.id.achievement3Image)
+
+        val dailyTask1Image : ImageView = view.findViewById(R.id.dailyTask1Image)
+        val dailyTask2Image : ImageView = view.findViewById(R.id.dailyTask2Image)
+
         lifecycleScope.launch(Dispatchers.IO) {
             val database = GoalsDatabase.getDatabase(requireContext())
             val taskDao = database.taskDao()
@@ -71,13 +93,27 @@ class MotivationFragment : Fragment() {
             var levelUser = user.experience / 100
             var experienceUser = user.experience % 100
 
-            level.text = "Уровень: ${levelUser}"
-            experience.text = "Опыт: ${experienceUser}"
+            withContext(Dispatchers.Main) {
+                level.text = "Уровень: ${levelUser}"
+                experience.text = "${experienceUser}/100"
+                levelProgressBar.progress = experienceUser
+            }
+
 
             if (tasks.isNotEmpty()) {
                 tasks[0]?.let { task ->
                     withContext(Dispatchers.Main) {
                         task1ProgressTextView.text = "${task.progress}/3"
+                        task1ProgressBar.max = 3
+                        task1ProgressBar.progress=task.progress
+
+                        if (task.progress == task1ProgressBar.max) {
+                            dailyTask1Image.setImageResource(R.drawable.check_daily)
+                        } else {
+                            dailyTask1Image.setImageResource(R.drawable.uncheck_daily)
+                        }
+
+
                     }
                 }
             }
@@ -89,6 +125,14 @@ class MotivationFragment : Fragment() {
                 tasks[1]?.let { task ->
                     withContext(Dispatchers.Main) {
                         task2ProgressTextView.text = "${task.progress}/1"
+                        task2ProgressBar.max = 1
+                        task2ProgressBar.progress= task.progress
+
+                        if (task.progress == task2ProgressBar.max) {
+                            dailyTask2Image.setImageResource(R.drawable.check_daily)
+                        } else {
+                            dailyTask2Image.setImageResource(R.drawable.uncheck_daily)
+                        }
                     }
                 }
             }
@@ -97,6 +141,14 @@ class MotivationFragment : Fragment() {
                 tasks[2]?.let { task ->
                     withContext(Dispatchers.Main) {
                         achievement1ProgressTextView.text = "${task.progress}/3"
+                        achievement1ProgressBar.max = 3
+                        achievement1ProgressBar.progress = task.progress
+
+                        val saturation = if (achievement1ProgressBar.progress == achievement1ProgressBar.max) 1f else 0f
+                        val matrix = ColorMatrix()
+                        matrix.setSaturation(saturation)
+                        val filter = ColorMatrixColorFilter(matrix)
+                        achievement1Image.colorFilter = filter
                     }
                 }
             }
@@ -105,6 +157,14 @@ class MotivationFragment : Fragment() {
                 tasks[3]?.let { task ->
                     withContext(Dispatchers.Main) {
                         achievement2ProgressTextView.text = "${task.progress}/10"
+                        achievement2ProgressBar.max = 10
+                        achievement2ProgressBar.progress = task.progress
+
+                        val saturation = if (achievement2ProgressBar.progress == achievement2ProgressBar.max) 1f else 0f
+                        val matrix = ColorMatrix()
+                        matrix.setSaturation(saturation)
+                        val filter = ColorMatrixColorFilter(matrix)
+                        achievement2Image.colorFilter = filter
                     }
                 }
             }
@@ -113,10 +173,20 @@ class MotivationFragment : Fragment() {
                 tasks[4]?.let { task ->
                     withContext(Dispatchers.Main) {
                         achievement3ProgressTextView.text = "${task.progress}/50"
+                        achievement3ProgressBar.max = 50
+                        achievement3ProgressBar.progress = task.progress
+
+                        val saturation = if (achievement3ProgressBar.progress == achievement3ProgressBar.max) 1f else 0f
+                        val matrix = ColorMatrix()
+                        matrix.setSaturation(saturation)
+                        val filter = ColorMatrixColorFilter(matrix)
+                        achievement3Image.colorFilter = filter
                     }
                 }
             }
         }
+
+
     }
 
 
